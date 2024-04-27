181de89a3d76fcf1c411c71b2d34aae8b2047e83
sbdiv <143803108+sbdiv@users.noreply.github.com>
2024-04-27 15:38:41 +0300

    Changes to be committed: 	new file:   Personal_Assistant/Dockerfile 	new file:   Personal_Assistant/Personal_Assistant/__pycache__/urls.cpython-312.pyc 	modified:   Personal_Assistant/Personal_Assistant/settings.py 	new file:   Personal_Assistant/db.sqlite3

--- Personal_Assistant/Personal_Assistant/settings.py
+++ Personal_Assistant/Personal_Assistant/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'Personal_Assistant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '567234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
