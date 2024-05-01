8d30b6320c6387bb50ffe48c36f9ef663a23968c
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-05-01 23:34:47 +0300

    Delete django_project-main directory

--- django_project-main/Personal_Assistant/News/currency_parser.py
+++
@@ -1,8 +0,0 @@
-# currency_parser.py
-from forex_python.converter import CurrencyRates
-
-def get_currency_rates():
-    currency_rates = CurrencyRates()
-    usd_to_eur = currency_rates.get_rate('USD', 'EUR')
-    eur_to_usd = currency_rates.get_rate('EUR', 'USD')
-    return {'USD_to_EUR': usd_to_eur, 'EUR_to_USD': eur_to_usd}
