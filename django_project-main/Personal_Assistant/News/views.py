babb3974371fe51ae807f961a7088571827c022d
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-05-01 23:39:26 +0300

    Add files via upload

---
+++ django_project-main/Personal_Assistant/News/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .news_parser import parse_news
+from .currency_parser import get_currency_rates
+
+def display_requests_page(request):
+    news = parse_news()
+    currency_rates = get_currency_rates()
+    return render(request, 'requests_page.html', {'news': news, 'currency_rates': currency_rates})
