bcfbf225b5dc0b5cd4a19d2f7fda013f7ce98a8f
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-05-07 20:32:12 +0300

    Add files via upload

---
+++ Contacts/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'contacts'
+
+urlpatterns = [
+    path('', views.contacts, name='contacts'),
+    path('contacts_list', views.contact_list, name='contact_list'),
+    path('new/', views.contact_create, name='contact_create'),
+    path('<int:pk>/update/', views.contact_update, name='contact_update'),
+    path('<int:pk>/delete/', views.contact_delete, name='contact_delete'),
+    path('search/', views.contact_search, name='contact_search'),
+    path('birthdays/', views.upcoming_birthdays, name='upcoming_birthdays'),
+]
